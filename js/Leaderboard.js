27555e1c86908cd1e8b4b36dca0718d3023be764
Tanish2002 <tanishkhare@gmail.com>
2021-12-07 17:38:10 +0530

    Switch to backend side api calls instead of client side

--- js/Leaderboard.js
+++ js/Leaderboard.js
@@ -1,40 +1,16 @@
 var settings = {
-  "url": "https://api.codechef.com/oauth/token",
-  "method": "POST",
+  "url": "https://codechef-website-backend-production.up.railway.app/",
+  "method": "GET",
   "timeout": 0,
-  "headers": {
-    "content-Type": "application/json",
-  },
-  "data": JSON.stringify({
-    "grant_type": "client_credentials",
-    "scope": "public",
-    "client_id": "",
-    "client_secret": "",
-    "redirect_uri": ""
-  }),
 };
-
 $.ajax(settings).done(function (response) {
-  authcode = response.result.data.access_token;
-  var settings2 = {
-    "url": "https://api.codechef.com/ratings/all?fields=rating&country=India&institution=SRM%20Institute%20Of%20Science%20And%20Technology&sortBy=rating&sortOrder=desc&limit=99",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "Accept": "application/json",
-      "Authorization": "Bearer " + authcode,
-    },
-  };
-
-  $.ajax(settings2).done(function (response) {
-    var row = '';
-    for (var i = 0; i < response.result.data.content.length; i++) {
-      row += '<tr>';
-      row += '<td>' + (i+1) + '.</td>';
-      row += '<td>' + response.result.data.content[i].fullname + '</td>';
-      row += '<td>' + response.result.data.content[i].rating + '</td>';
-      row += '</tr>';
-    }
-    $('#tbody').append(row);
-  });
+  var row = '';
+  for (var i = 0; i < response.result.data.content.length; i++) {
+    row += '<tr>';
+    row += '<td>' + (i+1) + '.</td>';
+    row += '<td>' + response.result.data.content[i].fullname + '</td>';
+    row += '<td>' + response.result.data.content[i].rating + '</td>';
+    row += '</tr>';
+  }
+  $('#tbody').append(row);
 });
